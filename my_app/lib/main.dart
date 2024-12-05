dcaa813b30d39ef38fa41b95331f1fbc28d7e28b
Bhooomika1 <bhooomikapal10@gmail.com>
2024-12-05 13:50:21 +0530

    new update

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -11,11 +11,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello world!'),
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen()
+      
     );
   }
 }
---
+++ my_app/lib/screens/home_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        systemOverlayStyle: const SystemUiOverlayStyle(
+          statusBarBrightness: Brightness.dark,
+        ),
+      ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(40, 1.2 * kToolbarHeight, 40, 20),
+        child: SizedBox(),
+      ),
+    );
+  }
+}
